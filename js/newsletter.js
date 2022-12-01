447aa0bfec06249c0c1bac59ea43857f7a635aa2
Oliver Rodger(Hongxin Ouyang) <1250523689@qq.com>
2022-12-01 17:21:55 -0600

    Greeting Functionality

---
+++ js/newsletter.js
@@ -0,0 +1,13 @@
+//function that deal with the onclick event
+let txtName=document.getElementById("greeting");
+function inputName(){
+    let name="";
+    let indication="Hellow, what's your name?"
+    while(name==="" || name.length < 1 || name.length>20 ){
+        name=prompt(indication,"guest");
+        indication="Please notices that name can not be empty. Meanwhile, it should less than 20 characters";
+    }
+    txtName.innerHTML="Hello, <span>"+name+"</span>";
+
+    txtName.style.display="inline-block";
+}
