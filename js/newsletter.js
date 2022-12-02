92ccfb0c3bf2ea2f01d0c2c807ae72fb72870cd2
prophet12505 <1250523689@qq.com>
2022-12-01 18:33:01 -0600

    Greeting Functionality update

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,17 +1,18 @@
 //function that deal with the onclick event
 let txtName=$("#greeting");
 let button=$("#enter-name-button");
-button.onclick=inputName;
+button.click(inputName);
 
 function inputName(){
+  
     let name="";
     let indication="Hellow, what's your name?"
     while(name==="" || name.length < 1 || name.length>20 ){
         name=prompt(indication,"guest");
         indication="Please notices that name can not be empty. Meanwhile, it should less than 20 characters";
     }
-    txtName.innerHTML="Hello, <span>"+name+"</span>";
+    txtName.html("Hello, <span>"+name+"</span>");
 
-    txtName.style.display="inline-block";
+    txtName.style("display:inline-block");
 }
-// inputName();
+
