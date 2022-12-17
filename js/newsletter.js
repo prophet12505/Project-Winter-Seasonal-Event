1331ba7539ef86d5af0081a4498645d5aa5ee5bf
prophet12505 <1250523689@qq.com>
2022-12-16 21:08:01 -0600

    deleted redundent comments

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,21 +1,3 @@
-//function that deal with the onclick event
-// let txtName=$("#greeting");
-// let button=$("#enter-name-button");
-// button.click(inputName);
-
-// function inputName(){
-  
-//     let name="";
-//     let indication="Hellow, what's your name?"
-//     while(name==="" || name.length < 1 || name.length>20 ){
-//         name=prompt(indication,"guest");
-//         indication="Please notices that name can not be empty. Meanwhile, it should less than 20 characters";
-//     }
-//     txtName.html("Hello, <span>"+name+"</span>");
-
-//     txtName.style("display:inline-block");
-// }
-
 
 btnName = document.getElementById("enter-name-button");
 userName = document.getElementById("name")
